eac5fe532a06e77fd5bf8a4f20c409b06342bd3a
Hugo Latendresse <hugolatendresse@gmail.com>
2023-12-28 19:20:35 -0500

    HuffmanCodes

--- compress.c
+++ compress.c
@@ -17,7 +17,6 @@ typedef struct HuffmanTree {
     Node **array;
 } HuffmanTree;
 
-// Function prototypes
 Node* newNode(char data, unsigned freq);
 HuffmanTree* createHuffmanTree(unsigned capacity);
 void swapNode(Node** a, Node** b);
@@ -40,4 +39,109 @@ int main() {
 }
 
 // Implement the functions here
-// ...
+Node* newNode(char data, unsigned freq) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
+}
+
+HuffmanTree* createHuffmanTree(unsigned capacity) {
+    HuffmanTree* minHeap = (HuffmanTree*)malloc(sizeof(HuffmanTree));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (Node**)malloc(minHeap->capacity * sizeof(Node*));
+    return minHeap;
+}
+
+void swapNode(Node** a, Node** b) {
+    Node* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void minHeapify(HuffmanTree* minHeap, int idx) {
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq) {
+        smallest = left;
+    }
+    
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq) {
+        smallest = right;
+    }
+    
+    if (smallest != idx) {
+        swapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+int isSizeOne(HuffmanTree* minHeap) {
+    return (minHeap->size == 1);
+}
+
+Node* extractMin(HuffmanTree* minHeap) {
+    Node* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+
+void insertHuffmanTree(HuffmanTree* minHeap, Node* minHeapNode) {
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq) {
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    minHeap->array[i] = minHeapNode;
+}
+
+void buildHuffmanTree(HuffmanTree* minHeap) {
+    Node* left, *right, *top;
+    while (!isSizeOne(minHeap)) {
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertHuffmanTree(minHeap, top);
+    }
+}
+
+void printCodes(Node* root, int arr[], int top) {
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+    
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+    
+    if (!root->left && !root->right) {
+        printf("%c: ", root->data);
+        for (int i = 0; i < top; ++i) {
+            printf("%d", arr[i]);
+        }
+        printf("\n");
+    }
+}
+
+void HuffmanCodes(char data[], int freq[], int size) {
+    HuffmanTree* minHeap = createHuffmanTree(size);
+    for (int i = 0; i < size; ++i) {
+        minHeap->array[i] = newNode(data[i], freq[i]);
+    }
+    minHeap->size = size;
+    buildHuffmanTree(minHeap);
+    int arr[100], top = 0;
+    printCodes(minHeap->array[0], arr, top);
+}
+
